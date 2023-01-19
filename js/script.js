6b81f9efa1dcaddb1b858cdda35474b4262bec11
SoHailaNekHat <sohaila.wb@gmail.com>
2023-01-19 12:42:32 +0430

    the header , footer and some parts of the main are done'

---
+++ js/script.js
@@ -0,0 +1,33 @@
+searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+
+}
+
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+  loginForm.classList.remove('active');
+}
+window.onscroll= () =>{
+  searchForm.classList.remove('active');
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }
+  else{
+document.querySelector('.header .header-2').classList.remove('active');
+  }
+}
+window.onload= () =>{
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }
+  else{
+document.querySelector('.header .header-2').classList.remove('active');
+  }
+}
