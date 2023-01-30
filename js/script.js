44fb055e2e833655e14ea7e37408fc8ef96b588a
SoHailaNekHat <sohaila.wb@gmail.com>
2023-01-30 11:04:38 +0430

    the featured section added

--- js/script.js
+++ js/script.js
@@ -31,3 +31,63 @@ window.onload= () =>{
 document.querySelector('.header .header-2').classList.remove('active');
   }
 }
+
+
+
+
+var swiper = new Swiper(".books-slider", {
+     
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+      delay:9500,
+      disableOnInteraction:false,
+    },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+         
+        },
+        768: {
+          slidesPerView: 2,
+        
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+    });
+
+
+
+
+var swiper = new Swiper(".featured-slider", {
+     spaceBetween:10,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+      delay:9500,
+      disableOnInteraction:false,
+    },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+         
+        },
+        450: {
+          slidesPerView: 2,
+        
+        },
+        768: {
+          slidesPerView: 3,
+          
+        },
+        1024: {
+          slidesPerView:4 ,
+          
+        },
+      },
+    });
+ 
+
